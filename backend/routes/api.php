c95ec46e9833c3c74bdb4a954b22716bc1f2010c
DylanRh2128 <140906098+DylanRh2128@users.noreply.github.com>
2026-02-04 12:13:08 -0500

    Conexion Laravel con React

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -5,7 +5,10 @@ use App\Http\Controllers\UsuarioController;
 use App\Http\Controllers\AgenteController;
 use App\Http\Controllers\InmobiliariaController;
 use App\Http\Controllers\PropiedadController;
+use App\Http\Controllers\AuthController;
 
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/register', [AuthController::class, 'register']);
 Route::apiResource('usuarios', UsuarioController::class);
 Route::apiResource('agentes', AgenteController::class);
 Route::apiResource('inmobiliarias', InmobiliariaController::class);
